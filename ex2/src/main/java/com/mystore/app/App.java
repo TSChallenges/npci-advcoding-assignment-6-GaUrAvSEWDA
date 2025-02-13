1550073391d1e0d1dacccf827a80578c74f0f8ca
GaUrAvSEWDA <sewdagaurav@gmail.com>
2025-02-13 21:24:57 +0530

    ex2 done

--- ex2/src/main/java/com/mystore/app/App.java
+++ ex2/src/main/java/com/mystore/app/App.java
@@ -1,16 +1,29 @@
 package com.mystore.app;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class App {
     public static void main(String[] arg) {
+      
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         
-        Product p1 = new Product();
-        p1.setId(1);
-        p1.setName("Tumbler");
-        System.out.println(p1);
 
-        Product p2 = new Product();
-        p2.setId(2);
-        p2.setName("Bucket");        
-        System.out.println(p2);
+       Product p1 = context.getBean("tumbler", Product.class);  
+       Product p2 = context.getBean("bucket", Product.class);
+
+       System.out.println(p1);
+       System.out.println(p2);
+
+
+        // Product p1 = new Product();
+        // p1.setId(1);
+        // p1.setName("Tumbler");
+        // System.out.println(p1);
+
+        // Product p2 = new Product();
+        // p2.setId(2);
+        // p2.setName("Bucket");        
+        // System.out.println(p2);
     }  
 }
